a9d4aec6ecdfdf00560ab391eaf63250556f9240
Olli Haapasalo <olli.haapasalo@hotmail.com>
2023-11-08 23:23:15 +0200

    viikko2 tehtävä 2 valmis

--- viikko2/project-reader/src/project.py
+++ viikko2/project-reader/src/project.py
@@ -1,17 +1,27 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
+    def __init__(self, name, description, license_,authors, dependencies, dev_dependencies):
         self.name = name
         self.description = description
+        self.license = license_
+        self.authors = authors
         self.dependencies = dependencies
         self.dev_dependencies = dev_dependencies
 
     def _stringify_dependencies(self, dependencies):
-        return ", ".join(dependencies) if len(dependencies) > 0 else "-"
+        #return ", ".join(dependencies) if len(dependencies) > 0 else "-"
+        return "\n- ".join(dependencies) if len(dependencies) > 0 else "-"
 
     def __str__(self):
         return (
             f"Name: {self.name}"
             f"\nDescription: {self.description or '-'}"
-            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
-            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
+            f"\nLicense: {self.license}"
+            f"\n"
+            f"\n"
+            f"Authors: \n- {self._stringify_dependencies(self.authors)}"
+            f"\n"
+            f"\n"
+            f"Dependencies: \n- {self._stringify_dependencies(self.dependencies)}"
+            f"\n"
+            f"\nDevelopment dependencies: \n- {self._stringify_dependencies(self.dev_dependencies)}"
         )
--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -12,9 +12,15 @@ class ProjectReader:
         content = request.urlopen(self._url).read().decode("utf-8")
         #print(content)
         parsed_toml = toml.loads(content)
-        print(parsed_toml)
+        #print(parsed_toml)
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
         #return Project("Test name", "Test description", [], [])
-        return Project(parsed_toml["tool"]["poetry"]["name"], parsed_toml["tool"]["poetry"]["description"],
-            parsed_toml["tool"]["poetry"]["dependencies"], parsed_toml["tool"]["poetry"]["group"]["dev"]["dependencies"])
+        name = parsed_toml["tool"]["poetry"]["name"]
+        description = parsed_toml["tool"]["poetry"]["description"]
+        license_ = parsed_toml["tool"]["poetry"]["license"]
+        authors = parsed_toml["tool"]["poetry"]["authors"]
+        dependencies = parsed_toml["tool"]["poetry"]["dependencies"]
+        dev_dependencies = parsed_toml["tool"]["poetry"]["group"]["dev"]["dependencies"]
+
+        return Project(name, description, license_,authors, dependencies, dev_dependencies)
