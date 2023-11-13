807df95e69d2983a6a23bb1883feef036604902e
Olli Haapasalo <olli.haapasalo@hotmail.com>
2023-11-13 18:19:28 +0200

    player.py update tehtävässä 9

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -5,6 +5,9 @@ class Player:
         self.team = dict['team']
         self.goals = dict['goals']
         self.assists = dict['assists']
+
+#lisäys kloonissa
+
  
     def __str__(self):
         return f"{self.name:25} {self.team:4} {self.goals:2} + {self.assists:2} = {self.goals+self.assists}"
