e59e71d3b137495a069c84101602f89c0a6dc310
Olli Haapasalo <olli.haapasalo@hotmail.com>
2023-12-08 19:04:45 +0200

    viikko6 tehtävä3

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All
+from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All, Or
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
@@ -15,25 +15,38 @@ def main():
     for player in stats.matches(matcher):
         print(player)
     print()
+    #matcher = And(
+    #    Not(HasAtLeast(2, "goals")),
+    #    PlaysIn("NYR")
+    #)
+
+    #for player in stats.matches(matcher):
+    #    print(player)
+
+    #print()
+    #matcher = And(
+    #    HasFewerThan(2, "goals"),
+    #    PlaysIn("NYR")
+    #)
+
+    #for player in stats.matches(matcher):
+    #    print(player)
+
+    #print()
+    #filtered_with_all = stats.matches(All())
+    #print(len(filtered_with_all))
+
+    print()
     matcher = And(
-        Not(HasAtLeast(2, "goals")),
-        PlaysIn("NYR")
+        HasAtLeast(70, "points"),
+        Or(
+        PlaysIn("NYR"),
+        PlaysIn("FLA"),
+        PlaysIn("BOS")
+        )
     )
 
     for player in stats.matches(matcher):
         print(player)
-
-    print()
-    matcher = And(
-        HasFewerThan(2, "goals"),
-        PlaysIn("NYR")
-    )
-
-    for player in stats.matches(matcher):
-        print(player)
-
-    print()
-    filtered_with_all = stats.matches(All())
-    print(len(filtered_with_all))
 if __name__ == "__main__":
     main()
