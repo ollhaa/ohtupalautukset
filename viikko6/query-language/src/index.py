639d45fb023428ceec9337aa380806db23e64286
Olli Haapasalo <olli.haapasalo@hotmail.com>
2023-12-09 16:46:20 +0200

    viikko6 tehtävä 4

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,7 @@
 from statistics import Statistics
 from player_reader import PlayerReader
 from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All, Or
+from query_builder import QueryBuilder
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
@@ -14,37 +15,12 @@ def main():
     )
     for player in stats.matches(matcher):
         print(player)
+    
     print()
-    #matcher = And(
-    #    Not(HasAtLeast(2, "goals")),
-    #    PlaysIn("NYR")
-    #)
 
-    #for player in stats.matches(matcher):
-    #    print(player)
+    query = QueryBuilder()
 
-    #print()
-    #matcher = And(
-    #    HasFewerThan(2, "goals"),
-    #    PlaysIn("NYR")
-    #)
-
-    #for player in stats.matches(matcher):
-    #    print(player)
-
-    #print()
-    #filtered_with_all = stats.matches(All())
-    #print(len(filtered_with_all))
-
-    print()
-    matcher = And(
-        HasAtLeast(70, "points"),
-        Or(
-        PlaysIn("NYR"),
-        PlaysIn("FLA"),
-        PlaysIn("BOS")
-        )
-    )
+    matcher = query.playsIn("NYR").hasAtLeast(22, "goals").build()
 
     for player in stats.matches(matcher):
         print(player)
---
+++ viikko6/query-language/src/query_builder.py
@@ -0,0 +1,21 @@
+from matchers import All, HasAtLeast, HasFewerThan, Or, PlaysIn, Not, And
+
+class QueryBuilder:
+    def __init__(self, pino =None):
+        self.pino_olio = pino
+        if pino is None: self.pino_olio = All()
+
+    def playsIn(self, team):
+        #self.pino_olio = QueryBuilder(PlaysIn(team))
+        return QueryBuilder(And(self.pino_olio, PlaysIn(team)))
+
+    def hasAtLeast(self, value, attr):
+        #self.pino_olio = QueryBuilder(hasAtLeast(value, attr))
+        return QueryBuilder(And(self.pino_olio, HasAtLeast(value, attr)))
+
+    def hasFewerThan(self, value, attr):
+        #self.pino_olio = QueryBuilder(HasFewerThan(value, attr))
+        return QueryBuilder(And(self.pino_olio, HasFewerThan(value, attr)))
+
+    def build(self):
+        return self.pino_olio
